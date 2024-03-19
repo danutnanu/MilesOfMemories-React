a63ab8d6c9df26beba6d41efd4a28914a20aa828
danut nanu <125583127+danutnanu@users.noreply.github.com>
2024-03-19 14:39:00 +0000

    Add path Story

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Navbar from './components/Navbar'
 import './App.css'
 import Home from './components/pages/Home'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Story from './components/pages/Story';
 // import ReactDOM from 'react-dom/client'
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
         <Navbar />
         <Routes>
           <Route path='/' element={<Home />} />
+          <Route path='/story' element={<Story />} />
         </Routes>
       </Router>
     </>
