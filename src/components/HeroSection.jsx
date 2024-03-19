c0ed9b46725783c97a7dc631ebef6e477a265169
danut nanu <125583127+danutnanu@users.noreply.github.com>
2024-03-19 14:39:52 +0000

    Delete border

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -7,13 +7,13 @@ function HeroSection() {
   return (
     <div className='hero-container w-100 border border-warning'>
       <video src="./video1.mp4" autoPlay loop muted className='w-100 h-100 position-fixed' />
-      <div className='hero-first m-5 pb-5 border border-warning'>
+      <div className='hero-first m-5 pb-5 '>
         <p className='h3'>Developed by: Danut Nanu</p>
         <p className='h3'>Designed by: Danut Nanu</p>
         <p className='h3'>Runner: Danut Nanu</p>
       </div>
 
-      <div className='hero-second text-center pt-5 m-5 border border-warning'>
+      <div className='hero-second text-center pt-5 m-5'>
         <h1 className='text-warning h1'>"EVERYTHING</h1>
         <p className='h1'>YOU WANTED TO KNOW ABOUT YOURSELF</p>
         <p className='h2'>YOU CAN LEARN IN</p>
