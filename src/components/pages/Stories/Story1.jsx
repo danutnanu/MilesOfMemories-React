7b127beed544fab8b4aac07fbb9034665d72b93d
danut nanu <125583127+danutnanu@users.noreply.github.com>
2024-03-20 07:50:39 +0000

    Add Story1.jsx

---
+++ src/components/pages/Stories/Story1.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Story.css";
+
+function Story1() {
+
+  return (
+    <>
+      <h1 className="text-primary text-center h1">Story1</h1>
+      <p className="text-secondary text-center h3">Wellcome to my empty page</p>
+    </>
+  );
+}
+
+export default Story1;
