a5c63ca42b41a52455e7b280615b0b5cb37318bf
danut nanu <125583127+danutnanu@users.noreply.github.com>
2024-03-20 07:50:56 +0000

    Add Story2.jsx

---
+++ src/components/pages/Stories/Story2.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import "./Story.css";
+
+function Story2() {
+
+  return (
+    <>
+      <h1 className="text-primary text-center h1">Story2</h1>
+      <p className="text-secondary text-center h3">Wellcome to my second empty page</p>
+    </>
+  );
+}
+
+export default Story2;
