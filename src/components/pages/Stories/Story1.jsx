895d7ae96244ebc7f8b2f6e0e067b6dc0a782401
danut nanu <125583127+danutnanu@users.noreply.github.com>
2024-04-11 12:35:04 +0100

    delete img

--- src/components/pages/Stories/Story1.jsx
+++ src/components/pages/Stories/Story1.jsx
@@ -7,6 +7,18 @@ function Story1() {
     <>
       <h1 className="text-primary text-center h1">STORY 1</h1>
       <p className="text-secondary text-center h3">Wellcome to my empty page</p>
+     <p className="text-secondary text-start ms-5 h4">Text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text </p>
+     <div className=' border border-dark'>
+                <img href='/#run2022'
+                  src="./run1.png run2.png"
+                  text="First Official Half Marathon - June 2022"
+                />
+                <a href='/#run2022'
+                  src='run2.png'
+                  text='First Marathon - October 2022'
+                  path='story2'
+                />
+          </div>
     </>
   );
 }
