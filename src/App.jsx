1743da4f9f96d0503a48caa9a9ff8da09fa7cdba
danut nanu <125583127+danutnanu@users.noreply.github.com>
2024-04-18 14:05:06 +0100

    Import Story6, Add  route path for story6

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import Story2 from './components/pages/Stories/Story2';
 import Story3 from './components/pages/Stories/Story3';
 import Story4 from './components/pages/Stories/Story4';
 import Story5 from './components/pages/Stories/Story5';
+import Story6 from './components/pages/Stories/Story6';
 // import ReactDOM from 'react-dom/client'
 
 function App() {
@@ -22,6 +23,7 @@ function App() {
           <Route path='/story3' element={<Story3 />} />
           <Route path='/story4' element={<Story4 />} />
           <Route path='/story5' element={<Story5 />} />
+          <Route path='/story6' element={<Story6 />} />
         </Routes>
       </Router>
     </>
