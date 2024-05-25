cc989a08b7161e3b45c292239850b6e212a9f8a0
danut nanu <125583127+danutnanu@users.noreply.github.com>
2024-05-25 09:37:09 +0100

    Update Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,42 +3,22 @@ import './Navbar.css'
 
 function Navbar() {
 
-  const [click, setClick] = useState(false);
-
-  const handleClick = () => setClick(!click);
-  const closeMobileMenu = () => setClick(false);
-
   return (
     <>
-      <nav className='navbar'>
-        <div className="navbar-container">
-          <div className="navbar-logo h1 mt-2 text-secondary" onClick={handleClick}>
-            RUN
-            <i className="fa-solid fa-person-running bg-warning rounded ms-2" />
-          </div>
-
-          {/* Hamburger Menu */}
-          <div className='menu-icon' onClick={handleClick}>
-            <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
-          </div>
-          {/* Hamburger Menu */}
-
-          <ul className={click ? 'nav-menu list-unstyled active' : 'nav-menu list-unstyled'}>
-            <li className='nav-item mt-2'>
-              <a href='/#' className='nav-links text-secondary' onClick={closeMobileMenu}>Home</a>
-            </li>
-            <li className='nav-item mt-2'>
-              <a href='/#run2022' className='nav-links text-secondary' onClick={closeMobileMenu}>Run 2022</a>
-            </li>
-            <li className='nav-item mt-2'>
-              <a href='/#run2023' className='nav-links text-secondary' onClick={closeMobileMenu}>Run 2023</a>
-            </li>
-            <li className='nav-item mt-2 align-items-right'>
-              <a href='/#run2024' className='nav-links text-secondary' onClick={closeMobileMenu}>Run 2024</a>
-            </li>
-          </ul>
-        </div>
-      </nav>
+      <ul class="nav nav-tabs sticky-top bg-body-tertiary">
+        <li class="nav-item">
+          <a class="nav-link active text-warning" aria-current="page" href="/#">RUN</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active text-secondary" href="/#run2022">Run 2022</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active text-secondary" href="/#run2023">Run 2023</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active text-secondary" href="/#run2024">Run 2024</a>
+        </li>
+      </ul>
     </>
   );
 }
