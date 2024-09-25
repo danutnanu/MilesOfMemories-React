0183b9ad946d1cf83517947f5eb1937954a9d5ef
danut nanu <125583127+danutnanu@users.noreply.github.com>
2024-09-25 16:44:46 +0100

    Fix hamburger menu

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,25 +1,32 @@
-import React, { useState } from 'react'
-import './Navbar.css'
+import React, { useState } from 'react';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
 
-function Navbar() {
+function Navigation() {
+  const [expanded, setExpanded] = useState(false);
+
+  const handleLinkClick = () => {
+    setExpanded(false);
+  };
   return (
-    <>
-      <ul class="nav nav-tabs sticky-top bg-body-tertiary justify-content-end">
-        <li class="nav-item">
-          <a class="nav-link active text-warning" aria-current="page" href="/#">RUN</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link active text-secondary" href="/#run2022">Run 2022</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link active text-secondary" href="/#run2023">Run 2023</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link active text-secondary" href="/#run2024">Run 2024</a>
-        </li>
-      </ul>
-    </>
+    <Navbar expanded={expanded} onToggle={() => setExpanded(!expanded)} expand="lg" className="bg-body-tertiary fixed-top">
+      <Container>
+      <img src="/public/athletics.ico" alt="logo" width="30" height="24" className="d-inline-block align-text-top m-1" />
+      <a className="badge text-bg-light text-wrap fst-italic px-3 ms-1 me-5 border border-info" >RUN</a>
+        {/* <Navbar.Brand>RUN</Navbar.Brand> */}
+        <Navbar.Toggle aria-controls="basic-navbar-nav" onClick={() => setExpanded(!expanded)} />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="/#" onClick={handleLinkClick}>Home</Nav.Link>
+            <Nav.Link href="#run2022" onClick={handleLinkClick}>Run 2022</Nav.Link>
+            <Nav.Link href="#run2023" onClick={handleLinkClick}>Run 2023</Nav.Link>
+            <Nav.Link href="#run2024" onClick={handleLinkClick}>Run 2024</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   );
-};
+}
 
-export default Navbar;
+export default Navigation;
